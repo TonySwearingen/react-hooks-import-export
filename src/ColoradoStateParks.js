b4973b4aca4f7e4ae87137db05938fa4a09a198f
unlimited1974@aol.com <unlimited1974@aol.com>
2022-02-21 13:38:20 -0600

    commit tree 1cf0dd40b25f27367dda686c219c8023c7c514ac

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,18 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  howManyParks();
 
-  return <h1>Colorado State Parks!</h1>;
-}
+  return (
+  <div>
+    <h1>Colorado State Parks!</h1>;
+    {/* <howManyParks/>;
+    <MesaVerde/>;
+    <RMFunctions.trees/>;
+    <RMFunctions.wildlife/>; */}
+  </div>
+  )}
+export default ColoradoStateParks;
